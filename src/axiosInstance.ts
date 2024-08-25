ba1d493e98fd924b54fece33490832eb0f65bb2f
artem <vadis1766@gmail.com>
2024-08-25 18:28:58 +0300

    Updated URLs to axios domain

--- src/axiosInstance.ts
+++ src/axiosInstance.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = import.meta.env.VITE_API_URL || 'https://travel-app-xxkltu.web.app/';
+const API_URL = import.meta.env.VITE_API_URL || 'https://simi129.github.io/TMA_NEW/';
 
 const instance = axios.create({
   baseURL: API_URL,
