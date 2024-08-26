4fa96687f9e69b11aeee2cbf8b72a400ecfa7d5a
artem <vadis1766@gmail.com>
2024-08-26 19:29:59 +0300

    Add dist folder for GitHub Pages deployment2

--- tma-backend/src/proxy.middleware.ts
+++ tma-backend/src/proxy.middleware.ts
@@ -1,7 +1,7 @@
 import { Injectable, NestMiddleware, Logger } from '@nestjs/common';
 import { Request, Response, NextFunction } from 'express';
 import * as httpProxy from 'http-proxy';
-import { IncomingMessage, ServerResponse } from 'http';
+import { ServerResponse } from 'http';
 
 @Injectable()
 export class ProxyMiddleware implements NestMiddleware {
@@ -11,17 +11,17 @@ export class ProxyMiddleware implements NestMiddleware {
   constructor() {
     this.proxy = httpProxy.createProxyServer({});
 
-    this.proxy.on('proxyReq', (proxyReq, req, res, options) => {
+    this.proxy.on('proxyReq', (proxyReq, _req, _res, options) => {
       this.logger.log(`Proxying request to: ${options.target}${proxyReq.path}`);
     });
 
-    this.proxy.on('proxyRes', (proxyRes, req, res) => {
+    this.proxy.on('proxyRes', (proxyRes, _req, _res) => {
       proxyRes.headers['Access-Control-Allow-Origin'] = '*';
       proxyRes.headers['Access-Control-Allow-Methods'] = 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS';
       proxyRes.headers['Access-Control-Allow-Headers'] = 'Origin,X-Requested-With,Content-Type,Accept,Authorization';
     });
 
-    this.proxy.on('error', (err, req, res: ServerResponse) => {
+    this.proxy.on('error', (err, _req, res: ServerResponse) => {
       this.logger.error(`Proxy error: ${err}`);
       if (!res.headersSent) {
         res.writeHead(500, { 'Content-Type': 'text/plain' });
@@ -30,7 +30,7 @@ export class ProxyMiddleware implements NestMiddleware {
     });
   }
 
-  use(req: Request, res: Response, next: NextFunction) {
+  use(req: Request, res: Response, _next: NextFunction) {
     const targetUrl = new URL(req.url.replace('/api/proxy/', ''));
     
     if (targetUrl.hostname === 'github.com') {
