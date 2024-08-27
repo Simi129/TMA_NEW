d4a609fea0b50dc913c73b4bf96623433b0aa2fd
artem <vadis1766@gmail.com>
2024-08-27 08:18:23 +0300

    Настройка деплоя на GitHub Pages3

--- tma-backend/src/main.ts
+++ tma-backend/src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   
   const corsOptions: CorsOptions = {
-    origin: ['https://walletbot.me', 'https://simi129.github.io', 'http://localhost:5173'],
+    origin: ['https://walletbot.me', 'https://simi129.github.io', 'https://faa8-46-109-142-185.ngrok-free.app'],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     allowedHeaders: 'Origin,X-Requested-With,Content-Type,Accept,Authorization',
     credentials: true,
