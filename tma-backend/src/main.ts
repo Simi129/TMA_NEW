bf40aa56e67528a385443691c876213000b4dabf
artem <vadis1766@gmail.com>
2024-08-27 08:46:01 +0300

    Настройка деплоя на GitHub Pages5

--- tma-backend/src/main.ts
+++ tma-backend/src/main.ts
@@ -1,9 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'path';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
   
   const corsOptions: CorsOptions = {
     origin: ['https://walletbot.me', 'https://simi129.github.io', 'https://faa8-46-109-142-185.ngrok-free.app'],
@@ -15,6 +17,9 @@ async function bootstrap() {
   };
 
   app.enableCors(corsOptions);
+
+  // Настройка статических файлов
+  app.useStaticAssets(join(__dirname, '..', 'public'));
 
   // Глобальный middleware для логирования запросов
   app.use((req, res, next) => {
