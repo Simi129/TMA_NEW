b5c420fc62fcf531545e99b8ee9866cb83a280e0
artem <vadis1766@gmail.com>
2024-08-29 07:29:04 +0300

    frie

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,5 +1,5 @@
 export const config = {
-  BACKEND_URL: 'https://72a7-46-109-142-185.ngrok-free.app',
+  BACKEND_URL: 'https://96ba-46-109-142-185.ngrok-free.app',
   MANIFEST_URL: 'https://simi129.github.io/TMA_NEW/tonconnect-manifest.json',
   STATIC_URL: 'https://simi129.github.io/TMA_NEW' // Добавьте эту строку
 };
